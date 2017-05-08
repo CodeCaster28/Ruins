069d5c6c234a43ba256d5ca1b984042299bc3291
Damian <spell2cast@gmail.com>
2017-05-08 22:30:19 +0200

    Healing pad that... heals player!

---
+++ Assets/Scripts/HealingPad.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingPad : MonoBehaviour {
+	private void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			other.transform.parent.GetComponent<Player>().ModifyHealth(100);
+		}
+	}
+}

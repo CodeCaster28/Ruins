e073cd0ea1958f74c9e56b510900def300669c97
Damian <spell2cast@gmail.com>
2017-07-16 12:37:35 +0200

    Monsters physics, fake shadows

--- Assets/Scripts/Weapons/Sword.cs
+++ Assets/Scripts/Weapons/Sword.cs
@@ -6,6 +6,7 @@ public class Sword : MonoBehaviour {
 
 	public Transform start;
 	public Transform end;
+	public Transform zeroZone;
 
 	private BaseLiving target;
 	private float knockoutForce = 10;
@@ -18,11 +19,28 @@ public class Sword : MonoBehaviour {
 
 	private void Update() {
 		RaycastHit hit;
-		if (Physics.Raycast(start.position, end.position - start.position, out hit, Vector3.Distance(start.position, end.position))) {
-			target = hit.collider.GetComponent<BaseLiving>();
-			if (target != null && charCtrl.IsAttacking) {
-				target.Damage(charCtrl.rBody.transform.position, knockoutForce, damage);
+
+		if (charCtrl.IsAttacking) {
+			if (Physics.Raycast(start.position, end.position - start.position, out hit, Vector3.Distance(start.position, end.position))) {
+				target = hit.collider.GetComponent<BaseLiving>();
+				if (target != null) {
+					target.Damage(charCtrl.rBody.transform.position, knockoutForce, damage);
+				}
+			}
+			else {
+				foreach (Collider col in Physics.OverlapCapsule(zeroZone.position - new Vector3(0, -0.4f, 0), zeroZone.position - new Vector3(0, 0.4f, 0), 0.25f)) {
+					target = col.GetComponent<BaseLiving>();
+					if (target != null) {
+						target.Damage(charCtrl.rBody.transform.position, knockoutForce, damage);
+					}
+				}
 			}
 		}
 	}
+
+	/*private void OnDrawGizmos() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(zeroZone.position - new Vector3(0, -0.4f, 0), 0.25f);
+		Gizmos.DrawWireSphere(zeroZone.position - new Vector3(0, 0.4f, 0), 0.25f);
+	}*/
 }
